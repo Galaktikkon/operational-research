867fb4ac309c3ea402b61ba16202da10239f84ea
Krzysztof Kapera <krzkapera@gmail.com>
2025-05-18 05:59:24 +0200

    create config

---
+++ main.py
@@ -0,0 +1,12 @@
+import sys
+import os
+import config
+
+sys.path.insert(0, os.path.abspath("src"))
+
+from src.main import main
+
+options = {
+    key: value for key, value in vars(config).items() if not key.startswith("__")
+}
+main(**options)
