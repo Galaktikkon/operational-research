3e7d9f87d768de50f8946f5cbdbecfa080ead3cc
Krzysztof Kapera <krzkapera@gmail.com>
2025-05-18 06:10:10 +0200

    fix couriers mutation

--- config.py
+++
@@ -1,6 +0,0 @@
-n_couriers = 4
-n_vehicles = 4
-n_packages = 100
-num_to_find = 8
-max_attempts = int(1e6)
-max_iter = 10000
--- src/ga/mutations/couriers.py
+++ src/ga/mutations/couriers.py
@@ -9,19 +9,20 @@ class CouriersMutation(Mutation):
     def _mutate_solution(self):
         z_j = self.solution.z_j
 
-        self.i1 = np.random.randint(self.problem.n_couriers)
+        drivers = np.unique(z_j)
+
+        self.i1 = np.random.choice(drivers)
         self.i2 = np.random.randint(self.problem.n_couriers)
-        while self.problem.n_couriers > 1 and self.i1 == self.i2:
+        while self.i1 == self.i2:
             self.i2 = np.random.randint(self.problem.n_couriers)
 
-        j1 = np.where(z_j == self.i1)[0]
-        self.j1 = j1[0] if j1.size else None
+        self.j1 = np.where(z_j == self.i1)[0][0]
 
         j2 = np.where(z_j == self.i2)[0]
         self.j2 = j2[0] if j2.size else None
 
-        if j1 is not None:
-            z_j[j1] = self.i2
+        if self.j1 is not None:
+            z_j[self.j1] = self.i2
         if j2 is not None:
             z_j[j2] = self.i1
 
