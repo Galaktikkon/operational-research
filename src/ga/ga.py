24ba68831211f16042147371a6d1b1caac67998a
Marek Małek <malekmarek56@gmail.com>
2025-05-31 15:04:19 +0200

    chore: add docs

--- src/ga/ga.py
+++ src/ga/ga.py
@@ -22,6 +22,27 @@ crossnok = 0
 
 
 class GA:
+    """
+    Genetic Algorithm for solving the problem of optimizing courier and vehicle assignments.
+    This class implements the genetic algorithm with crossover and mutation operations to find
+    optimal solutions for the given problem.
+    Attributes:
+        problem (Problem): The problem instance containing couriers, vehicles, and packages.
+        checker (SolutionChecker): An instance to check the feasibility of solutions.
+        C (float): A constant used in cost calculation.
+        alpha (float): A parameter used in delay cost calculation.
+        initial_population (list[Solution]): The initial population of solutions to start the algorithm.
+    Methods:
+        get_cost(solution: Solution) -> float:
+            Calculate the cost of a given solution.
+        crossover(s1: Solution, s2: Solution) -> tuple[Solution, Solution]:
+            Perform crossover between two solutions to create new solutions.
+        mutation(solution: Solution) -> Solution:
+            Apply mutation to a solution to create a new solution.
+        run(max_iter=1000) -> tuple[Solution, Solution]:
+            Execute the genetic algorithm to optimize the solution over a specified number of iterations.
+    """
+
     def __init__(self, problem: Problem, initial_population: list[Solution]):
         self.problem = problem
         self.checker = SolutionChecker(problem)
@@ -31,6 +52,16 @@ class GA:
 
     @functools.cache
     def get_cost(self, solution: Solution):
+        """
+        Calculate the cost of a solution.
+        The cost is calculated as follows:
+        - Rates of couriers: sum of hourly rates of couriers multiplied by the time spent
+        - Fuel cost: sum of fuel consumption of vehicles multiplied by the distance traveled
+        - Delay: alpha divided by the number of packages multiplied by the sum of delays
+
+        :param solution: Solution to calculate the cost for
+        :return: Total cost of the solution
+        """
         c_i = np.array([c.hourly_rate / 60 for c in self.problem.couriers])
         p_j = np.array([v.fuel_consumption for v in self.problem.vehicles])
 
@@ -43,6 +74,18 @@ class GA:
         return rates + fuel_cost + delay
 
     def crossover(self, s1: Solution, s2: Solution):
+        """Perform crossover between two solutions.
+        This method attempts to create two new solutions by combining the couriers and vehicles
+        from the two parent solutions.
+
+        Args:
+            s1 (Solution): The first parent solution.
+            s2 (Solution): The second parent solution.
+
+        Returns:
+            tuple: A tuple containing two new solutions (a, b). If crossover fails after 10 attempts,
+                   returns (None, None).
+        """
         global crossok, crossnok
         for _ in range(10):
             a, b = self._crossover(s1, s2)
@@ -54,6 +97,18 @@ class GA:
         return None, None
 
     def _crossover(self, s1: Solution, s2: Solution):
+        """Internal method to perform crossover between two solutions.
+        This method combines the couriers and vehicles from two solutions while respecting the
+        permissions of couriers to vehicles. It creates two new solutions based on the input solutions.
+
+        Args:
+            s1 (Solution): The first solution.
+            s2 (Solution): The second solution.
+
+        Returns:
+            tuple: A tuple containing two new solutions (a, b). If either solution is infeasible,
+                   returns (None, None).
+        """
         problem = self.problem
 
         z_j1 = s1.z_j.copy()
@@ -62,6 +117,7 @@ class GA:
         z_j2 = s2.z_j.copy()
         y_k2 = s2.y_k.copy()
 
+        # Get used and unused and used couriers and vehicles from both solutions
         s1_used_couriers = np.unique(z_j1)
         s1_used_couriers = s1_used_couriers[s1_used_couriers != -1]
         s1_unused_couriers = np.setdiff1d(
@@ -74,6 +130,7 @@ class GA:
             np.arange(problem.n_couriers), s2_used_couriers, assume_unique=True
         )
 
+        # Get used vehicles from both solutions
         s1_used_vehicles = np.unique(y_k1)
         s2_used_vehicles = np.unique(y_k2)
 
@@ -95,6 +152,7 @@ class GA:
         s1_used_set = set(s1_used_couriers)
         s1_unused_set = set(s1_unused_couriers)
 
+        # Assign couriers to vehicles in a way that respects permissions
         for j in np.random.permutation(s1_used_vehicles):
             # TODO: sprawdzac czy trasa nie jest dluzsza niz czas pracy kuriera
             for _ in range(2 * len(s2_used_set)):
@@ -105,6 +163,7 @@ class GA:
                     a_z_j[j] = i
                     break
             else:
+                # If no used couriers are available, try unused couriers
                 for _ in range(2 * len(s2_unused_set)):
                     i = np.random.choice(list(s2_unused_set))
                     if (i, j) in problem.permissions and i not in a_set:
@@ -115,6 +174,7 @@ class GA:
                 else:
                     break
 
+        # Assign couriers to vehicles in b in a way that respects permissions
         for j in np.random.permutation(s2_used_vehicles):
             for _ in range(2 * len(s1_used_set)):
                 i = np.random.choice(list(s1_used_set))
@@ -124,6 +184,7 @@ class GA:
                     b_z_j[j] = i
                     break
             else:
+                # If no used couriers are available, try unused couriers
                 for _ in range(2 * len(s1_unused_set)):
                     i = np.random.choice(list(s1_unused_set))
                     if (i, j) in problem.permissions and i not in b_set:
@@ -146,6 +207,13 @@ class GA:
         return a, b
 
     def mutation(self, solution: Solution):
+        """Perform mutation on a solution.
+        This method applies various mutation strategies to the given solution.
+        Args:
+            solution (Solution): The solution to mutate.
+        Returns:
+            Solution: The mutated solution if a feasible mutation is found, otherwise the original solution.
+        """
         x_jv = solution.x_jv
         y_k = solution.y_k
         z_j = solution.z_j
@@ -180,6 +248,18 @@ class GA:
         return solution
 
     def run(self, max_iter=1000):
+        """Run the genetic algorithm to optimize the solution.
+        This method initializes the population, performs crossover and mutation operations,
+        and iteratively improves the solutions until the maximum number of iterations is reached.
+
+        Args:
+            max_iter (int, optional): The maximum number of iterations to run the algorithm. Defaults to 1000.
+
+        Returns:
+            tuple: A tuple containing the initial best solution and the final best solution found.
+                   The first element is the initial best solution, and the second element is the final best solution.
+        """
+
         solutions = self.initial_population
         l = len(solutions)  # noqa: E741
 
